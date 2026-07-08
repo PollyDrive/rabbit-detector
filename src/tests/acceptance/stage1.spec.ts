613a3ec0ce0f23e5b1a85bddbabdf814829ed7cf
Krvtzz <krvtzz@gmail.com>
2026-07-08 14:26:52 +0800

    fix(tests): update imports to tsx

--- src/tests/acceptance/stage1.spec.ts
+++ src/tests/acceptance/stage1.spec.ts
@@ -1,4 +1,4 @@
-import './stage1-shell.test.ts'
-import './stage1-zones.test.ts'
-import './stage1-popover.test.ts'
-import './stage1-panels.test.ts'
+import './stage1-shell.test.tsx'
+import './stage1-zones.test.tsx'
+import './stage1-popover.test.tsx'
+import './stage1-panels.test.tsx'
